69dcfe1227e701b9f936a05ec6390b897f84c970
Devan Rivera <devanrivera98@gmail.com>
2025-06-03 21:03:54 -0700

    added state css to highlight selected navitem

--- mindsoother/src/app/components/header/Header.tsx
+++ mindsoother/src/app/components/header/Header.tsx
@@ -1,20 +1,25 @@
+'use client'
 import React from 'react';
 import { LuBrain as LuBrainRaw } from 'react-icons/lu';
 import { LuHouse as LuHouseRaw } from "react-icons/lu";
 import { LuBookmark as LuBookmarkRaw} from "react-icons/lu";
 import NavLink from './components/NavLink';
+import { useState } from 'react';
 
 
 
 
 
 export default function Header() {
+    const [activeIndex, setActiveIndex] = useState(0);
 
+    const handleNavClick = (index: number) => {
+        setActiveIndex(index)
+    }
 
     const LuBrain = (props: React.ComponentProps<'svg'>) => <>{LuBrainRaw({...props})}</>
-    const LuHouse = () => <>{LuHouseRaw({size: 20})}</>
-    const LuBookmark = () => <>{LuBookmarkRaw({size: 20})}</>
-
+    const LuHouse = (props: React.ComponentProps<'svg'>) => <>{LuHouseRaw({...props})}</>
+    const LuBookmark = (props: React.ComponentProps<'svg'>) => <>{LuBookmarkRaw({...props})}</>
 
     return (
         <header className="shadow-md/10">
@@ -26,9 +31,9 @@ export default function Header() {
                     </div>
                 </div>
                 <div className='flex items-center gap-x-10'>
-                    <NavLink Icon={LuHouse} name="Home" isActive={false} />
-                    <NavLink Icon={LuBrain} name="Technique Explorer" fontSize={20} isActive={false} />
-                    <NavLink Icon={LuBookmark} name="Saved Technique" isActive={false}/>
+                    <NavLink Icon={LuHouse} name="Home" fontSize={20} isActive={activeIndex === 0} onClick={() => handleNavClick(0)}  />
+                    <NavLink Icon={LuBrain} name="Technique Explorer" fontSize={20} isActive={activeIndex === 1} onClick={() => handleNavClick(1)} />
+                    <NavLink Icon={LuBookmark} name="Saved Technique" fontSize={20} isActive={activeIndex === 2} onClick={() => handleNavClick(2)}/>
                     <button className='bg-[#4f45e4] text-white px-4 py-2 rounded-md'>
                         Sign In
                     </button>
--- mindsoother/src/app/components/header/components/NavLink.tsx
+++ mindsoother/src/app/components/header/components/NavLink.tsx
@@ -2,18 +2,19 @@ import React from "react";
 
 interface IconInterface {
     Icon: React.ElementType;
-    name: string,
+    name: string;
     fontSize?: number;
-    isActive: boolean
+    isActive: boolean;
+    onClick: () => void;
 }
 
-export default function NavLink({isActive, Icon, name, fontSize} :  IconInterface) {
+export default function NavLink({isActive, onClick, Icon, name, fontSize} :  IconInterface) {
 
     return (
-        <div className={`cursor-pointer p-1.5 rounded  ${isActive ? 'bg-indigo-50' : 'hover:bg-gray-50'}`}>
+        <div className={`cursor-pointer p-1.5 rounded  ${isActive ? 'bg-indigo-50' : 'hover:bg-gray-50'}`} onClick={onClick}>
             <a className="flex pointer">
             <Icon size={fontSize} />
-            <span className="pl-2 ">{name}</span>
+            <span className={`pl-2 hover:text-indigo-600 ${isActive ? 'text-indigo-600' : ''}`}>{name}</span>
             </a>
         </div>
     )
