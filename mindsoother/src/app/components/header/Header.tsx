4eb50dc44f0967b8e05bfdabdccd7df061ac4b8b
Devan Rivera <devanrivera98@gmail.com>
2025-06-04 21:03:47 -0700

    made mobile view for header

--- mindsoother/src/app/components/header/Header.tsx
+++ mindsoother/src/app/components/header/Header.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { LuBrain, LuHouse, LuBookmark, InformationCircle } from '../icons';
 import NavLink from './components/NavLink';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MobileMenu from './components/MobileMenu';
 
 
@@ -11,6 +11,21 @@ import MobileMenu from './components/MobileMenu';
 
 export default function Header() {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        const windowWatcher = () => {
+            if (window.innerWidth > 1023) {
+                setIsMenuOpen(false)
+            }
+        }
+        window.addEventListener('resize', windowWatcher)
+
+        return () => {
+            window.removeEventListener('resize', windowWatcher)
+        }
+    }, [])
 
     const handleNavClick = (index: number) => {
         setActiveIndex(index)
@@ -19,15 +34,15 @@ export default function Header() {
 
 
     return (
-        <header className="shadow-md/10">
-            <div className="flex justify-between mx-auto max-w-7xl lg:px-8 sm:px-6 px-4 py-4">
+        <header className="shadow-md/10 w-full">
+            <div className="flex justify-between mx-auto max-w-7xl lg:px-8 sm:px-6 px-4 py-4 ">
                 <div className='flex items-center cursor-pointer'>
                     <LuBrain fontSize={35} color={'#4f45e4'} />
                     <div className='pl-2 font-bold text-xl'>
                         MindSoother
                     </div>
                 </div>
-                <div className='hidden lg:flex items-center gap-x-7'>
+                <div className={`hidden lg:flex items-center gap-x-7 `}>
                     <NavLink Icon={LuHouse} name="Home" fontSize={20} isActive={activeIndex === 0} onClick={() => handleNavClick(0)}  />
                     <NavLink Icon={LuBrain} name="Technique Explorer" fontSize={20} isActive={activeIndex === 1} onClick={() => handleNavClick(1)} />
                     <NavLink Icon={LuBookmark} name="Saved Technique" fontSize={20} isActive={activeIndex === 2} onClick={() => handleNavClick(2)}/>
@@ -36,11 +51,21 @@ export default function Header() {
                         Sign In
                     </button>
                 </div>
-
-
                 {/* Mobile Menu Button */}
-                <MobileMenu />
+                <MobileMenu onClick={() => 1} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
             </div>
+            {/* Mobile Dropdown */}
+            <div className={`lg:hidden flex flex-col gap-y-2 px-2 ${isMenuOpen ? ' block ' : 'hidden'} `}>
+                    <NavLink Icon={LuHouse} name="Home" fontSize={20} isActive={activeIndex === 0} onClick={() => handleNavClick(0)}  />
+                    <NavLink Icon={LuBrain} name="Technique Explorer" fontSize={20} isActive={activeIndex === 1} onClick={() => handleNavClick(1)} />
+                    <NavLink Icon={LuBookmark} name="Saved Technique" fontSize={20} isActive={activeIndex === 2} onClick={() => handleNavClick(2)}/>
+                    <NavLink Icon={InformationCircle} name="About" fontSize={20} isActive={activeIndex === 3} onClick={() => handleNavClick(3)}/>                    
+                   <div className='mx-2 pb-4'>
+                   <button className='w-full cursor-pointer bg-[#4f45e4] text-white py-2 rounded-md'>
+                        Sign In
+                    </button>
+                   </div>
+                </div>
         </header>
     )
 }
--- mindsoother/src/app/components/header/components/MobileMenu.tsx
+++ mindsoother/src/app/components/header/components/MobileMenu.tsx
@@ -1,16 +1,15 @@
 'use client'
 import { useState } from "react"
-import { GiHamburgerMenu } from "../../icons";
+import { GiHamburgerMenu, AiOutlineClose } from "../../icons";
 
-export default function MobileMenu() {
+export default function MobileMenu({onClick, isMenuOpen, setIsMenuOpen} : {onClick: () => void, isMenuOpen : boolean, setIsMenuOpen: (arg: boolean) => void}) {
 
-    const [isActive, setIsActive] = useState(false);
     const [isFocused, setIsFocused] = useState(false);
 
     return (
-        <button className={`lg:hidden cursor-pointer p-2 rounded-md border ${isActive ? 'bg-indigo-50' : 'hover:bg-gray-50'} ${isFocused ? 'border-[#4f45e4]': 'border-transparent'}`} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
+        <button className={`lg:hidden cursor-pointer p-2 rounded-md border hover:bg-gray-50 ${isFocused ? 'border-[#4f45e4]': 'border-transparent'}`} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <div className="flex">
-        <GiHamburgerMenu fontSize={20}/>
+            {isMenuOpen  ? <AiOutlineClose fontSize={20} strokeWidth={50} /> :<GiHamburgerMenu fontSize={20} />}
         </div>
     </button>
     )
