252bbbebedfa79cd6bc218db0bd6033672a21805
Devan Rivera <devanrivera98@gmail.com>
2025-06-24 16:13:47 -0700

    customizable therapeutic card component is created and so is the layout for that section with the different therapy methods

--- mindsoother/src/app/home/page.tsx
+++ mindsoother/src/app/home/page.tsx
@@ -1,11 +1,13 @@
 import HowItWorks from "./HowItWorks";
 import IntroHome from "./IntroHome";
+import TherapeuticApproaches from "./TherapeuticApproaches";
 
 export default function Homepage() {
   return (
     <>
       <IntroHome />
       <HowItWorks />
+      <TherapeuticApproaches />
     </>
   );
 }
