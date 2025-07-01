63258775ea400e729008bbb0ea98a486967cc734
Devan Rivera <devanrivera98@gmail.com>
2025-07-01 13:38:26 -0700

    updated disclaimer

--- mindsoother/src/app/about/page.tsx
+++ mindsoother/src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { IoMdCheckmark } from "../components/icons";
+import { IoMdCheckmark, InformationCircle } from "../components/icons";
 
 export default function About() {
   return (
@@ -16,7 +16,7 @@ export default function About() {
               <div>
                 <h2 className="font-bold text-2xl">Our Mission</h2>
                 <p className="mt-4 text-faded-gray">
-                  TherapyTech Explorer was created to make evidence-based
+                  Mindsoother was created to make evidence-based
                   psychological techniques more accessible to everyone. We
                   believe that understanding the tools therapists use can help
                   people make progress even between therapy sessions, or while
@@ -76,8 +76,22 @@ export default function About() {
                   </li>
                 </ul>
               </div>
-              <div>
-                <h2>Important Disclaimer</h2>
+              <div className="mt-10">
+                <h2 className="font-bold text-2xl">Important Disclaimer</h2>
+                <div className="flex bg-orange-100 flex border-l-4 border-orange-400 gap-x-2 p-4 mt-4">
+                    <InformationCircle className="text-orange-300 flex-shrink-0" fontSize={25} />
+                    <div className="flex flex-col gap-y-2">
+                        <p className="text-orange-700 font-semibold">
+                        Mindsoother Explorer is intended for informational and self-guided exploration purposes only. It is not a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician, therapist, or other qualified health provider with any questions you may have regarding a mental health condition. Do not disregard professional advice or delay seeking it because of content or features on this site.
+                        </p>
+                        <p className="text-orange-700 font-semibold">
+                        If you are experiencing a crisis or emergency, contact a mental health professional, call 911, or visit the nearest emergency room immediately.
+                        </p>
+                        <p className="text-orange-700 font-bold">
+                        By using this site, you acknowledge and agree that Mindsoother Explorer and its creators are not responsible for any decisions or actions you take based on the content provided. Use of this site is at your own risk.
+                        </p>
+                    </div>
+                </div>
               </div>
             </div>
           </div>
