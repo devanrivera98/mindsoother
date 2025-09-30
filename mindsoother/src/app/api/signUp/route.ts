d8754085f05286e7effc8a02f118d6784fbcaeca
Devan Rivera <devanrivera98@gmail.com>
2025-09-29 19:41:40 -0700

    added admin auth and handled sign up request for when a user tries to make an account with an existing email

--- mindsoother/src/app/api/signUp/route.ts
+++ mindsoother/src/app/api/signUp/route.ts
@@ -1,22 +1,28 @@
-import { createClient } from "@/app/utils/supabase/server";
+import { adminAuthClient } from "@/app/utils/supabase/adminClient";
 import { NextResponse } from "next/server";
+import { use } from "react";
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
 
     try {
-    const supabase = await createClient();
     const {email} = await req.json()
 
     if (!email) {
         return NextResponse.json({error: "Missing Email"}, {status: 400})
     }
 
-    const {data, error} = await supabase.auth.admin.listUsers(); 
+    const {data, error} = await adminAuthClient.auth.admin.listUsers(); 
     if (error) throw error;
 
     const userExist = data.users.some((user) => user.email === email)
+    console.log(userExist)
 
-    return NextResponse.json({userExist: userExist})
+    if(userExist) {
+        return NextResponse.json({userExist: userExist, message: 'This email address is already in the system.'})
+    } else {
+        return NextResponse.json({userExist: userExist})
+    }
+
     } catch (err) {
         console.log('Response error', err)
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
