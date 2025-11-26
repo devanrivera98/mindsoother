0a40aeab8d3d32197a2bed9e188bfaf06cd25125
Devan Rivera <devanrivera98@gmail.com>
2025-11-26 12:33:26 -0800

    removed unused import from Header.tsx

--- mindsoother/src/app/components/header/Header.tsx
+++ mindsoother/src/app/components/header/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createClient } from "@/lib/supabase/server";
 import HeaderClient from "./HeaderClient";
 
 export default async function Header(): Promise<React.JSX.Element> {
